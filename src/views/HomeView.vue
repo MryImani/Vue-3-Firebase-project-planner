1706819fb4ed89fe4d935e36f9b3b64bc860e68c
maryam imani <maryam_imani@yahoo.com>
2022-09-03 14:43:04 +0430

    fetch project data  into home page by axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
-    hello
+    <div v-if="projects.length">
+      <div class="project" v-for="project in projects" :key="project.id">
+        <p>{{project.title}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
   name: 'HomeView',
   components: {
+  },
+  data(){
+    return {
+      projects : [],
+    }
+  },
+  mounted(){
+     axios.get(' http://localhost:3000/projects')
+     .then(res => this.projects = res.data)
+     .catch(err => console.log(err.message));
+
   }
 }
 </script>
